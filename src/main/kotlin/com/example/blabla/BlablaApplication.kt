ac929487eec99cc078c47bed452e48ea242cb878
Murad <mrd.babayev117@gmail.com>
2024-10-25 15:49:30 +0400

    salam

---
+++ src/main/kotlin/com/example/blabla/BlablaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.blabla
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlablaApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlablaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/blabla/BlablaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.blabla
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlablaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
